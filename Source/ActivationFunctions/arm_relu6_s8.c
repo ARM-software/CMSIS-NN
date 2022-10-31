6f1f56c7e90038fda4a371b46e2c82b4dfc42e1f
Felix Thomasmathibalan <felixjohnny.thomasmathibalan@arm.com>
2022-10-31 16:50:11 +0100

    Review: Update copyright header
    Change-Id: I730e5505d87521d2e3927c127189cb486ff12b94

--- Source/ActivationFunctions/arm_relu6_s8.c
+++ Source/ActivationFunctions/arm_relu6_s8.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2019 Arm Limited or its affiliates. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright 2010-2019, 2022 Arm Limited and/or its affiliates <open-source-office@arm.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  *
